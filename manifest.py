7db421dbafe8a9bb2b7a1a5dc67b73c2507ff4b3
simoncarlson <cimon.carlson@gmail.com>
2019-02-04 11:27:35 +0100

    Minimal input handling

--- manifest.py
+++ manifest.py
@@ -18,17 +18,17 @@ def get_format(image_path):
 
 def generate_json(args, file=None):
     json_data = {}
+    conditions = []
     if file is not None:
         with open(file, "r") as f:
             f = json.load(f)
             # Assume the version of manifest is the same since user wants to build upon manifest
             json_data["versionID"] = f["versionID"]
-            conditions = []
             # Copy existing conditions as the manifest targets same device
             for entry in f["conditions"]:
                 conditions.append(entry)
     
-    if json_data["versionID"] is None:
+    if file is None:
         json_data["versionID"] = args["m"]
 
     timestamp = time.time()
@@ -45,22 +45,22 @@ def generate_json(args, file=None):
     if 0 not in present_conditions:
         # generate UUID5 from vendor dns
         vendorID = uuid.uuid5(uuid.NAMESPACE_DNS, "test.com")
-        json_data["conditions"].append({"type":0,"UUID":vendorID})
+        json_data["conditions"].append({"type":0,"UUID":str(vendorID)})
     elif 0 in present_conditions:
         # Convert it to UUID so that it can be used as namespace for other UUIDs
         vendorID = uuid.UUID(json_data["conditions"][0]["UUID"])
 
     if 1 not in present_conditions:
         # generate UUID5 from vendorID, get from previous or manifest
-        classID = uuid.uuid5(vendorID, "class1")
-        json_data["conditions"].append({"type":1,"UUID":classID})
+        classID = uuid.uuid5(vendorID, args["c"])
+        json_data["conditions"].append({"type":1,"UUID":str(classID)})
     elif 1 in present_conditions:
         classID = uuid.UUID(json_data["conditions"][1]["UUID"])
 
     if 2 not in present_conditions and args["d"] is not None:
         # generate UUID5 from classID
-        deviceID = uuid.uuid5(classID, "device1")
-        json_data["conditions"].append({"type":2,"UUID":deviceID})
+        deviceID = uuid.uuid5(classID, args["d"])
+        json_data["conditions"].append({"type":2,"UUID":str(deviceID)})
 
     json_data["digests"] = []
     with open(args["i"], "rb") as f:
@@ -69,6 +69,37 @@ def generate_json(args, file=None):
     json_data["digests"].append({"URI":args["u"],"digest":image_digest})
     
     return json_data
+
+def validate_input(args):
+    if args["f"] is not None and not os.path.isfile(args["f"]):
+        print("Could not find manifest.")
+        sys.exit(1)
+    elif args["f"] is not None and pathlib.Path(args["f"]).suffix != ".json":
+        print("Manifest must be a JSON file.")
+        sys.exit(1)
+    if args["i"] is None:
+        print("Image file is needed.")
+        sys.exit(1)
+    elif not os.path.isfile(args["i"]):
+        print("Could not find image file.")
+        sys.exit(1)
+
+    if args["f"] is None:
+        if args["v"] is None:
+            print("Vendor namespace is needed for vendor ID.")
+            sys.exit(1)
+        if args["c"] is None:
+            print("Class identifier is needed for class ID.")
+            sys.exit(1)
+
+    if args["u"] is None:
+        print("URI for the image is needed.")
+        sys.exit(1)
+
+    if args["m"] is None:
+        print("Manifest version is needed")
+        sys.exit(1)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Create a manifest.")
@@ -82,16 +113,7 @@ if __name__ == "__main__":
     parser.add_argument("-u", metavar="URI", type=str)
     args = vars(parser.parse_args())
     
-    if args["f"] is None or not os.path.isfile(args["f"]):
-        print("Could not find manifest.")
-        sys.exit(1)
-    elif pathlib.Path(args["f"]).suffix != ".json":
-        print("Manifest must be a JSON file.")
-        sys.exit(1)
-    if args["i"] is None or not os.path.isfile(args["i"]):
-        print("Could not find image.")
-        sys.exit(1)
-    
+    validate_input(args)    
 
     json_data = generate_json(args, file=args["f"])
     cbor_data = cbor.dumps(json_data)
